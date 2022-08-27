6e7ac802ec1e0e539c930ac0b783885d285210f5
Vitaly Shatalov <vetka921@gmail.com>
2022-08-27 22:24:31 +0700

    feat(FOX-3): Categorize interests

--- src/services/schema/schema.controller.ts
+++ src/services/schema/schema.controller.ts
@@ -3,6 +3,24 @@ import { createController } from '@/common/createController'
 import enEnums from '@/domain/enums/translations/en'
 import ruEnums from '@/domain/enums/translations/ru'
 import { Controller } from '@/types/controllerRelated.types'
+import { groupedInterests } from '@/domain/groupedInterests'
+import { justLog } from '@/utils/justLog'
+
+const categoryPairs = Object.fromEntries(
+  Object.entries(groupedInterests).flatMap(([group, interests]) =>
+    interests.map(interest => [interest, group]),
+  ),
+)
+
+const preparedRuDictionary = {
+  ...ruEnums,
+  Interests: Object.fromEntries(
+    Object.entries(ruEnums.Interests).map(([key, value]) => [
+      key,
+      { value: value, category: categoryPairs[key] ?? 'unknown' },
+    ]),
+  ),
+}
 
 export const registerSchemaController: Controller = createController({
   scope: 'schema',
@@ -20,9 +38,24 @@ export const registerSchemaController: Controller = createController({
       (resolve, reject, context) =>
         async ({ lang: passedLang }) => {
           const lang = passedLang || 'ru'
+          const dictionary = lang === 'ru' ? preparedRuDictionary : enEnums
 
-          return resolve({ enums: lang === 'ru' ? ruEnums : enEnums })
+          return resolve({
+            enums: dictionary,
+          })
         },
+    )
+
+    addListener(
+      {
+        eventName: 'getInterests',
+        description: 'Get grouped interests (GroupKey - InterestKey[])',
+        requireAuth: false,
+        restMethods: ['POST', 'GET'],
+      },
+      (resolve, reject, context) => () => {
+        return resolve(groupedInterests)
+      },
     )
   },
 })
